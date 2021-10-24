1539e011b595f3a2196de66a8a03adaef760377f
Thomas Simonsen <thom622g@stud.kea.dk>
2021-10-24 09:30:11 +0200

    did soem sit

--- 0._assignments/3._sess7/carObject.py
+++ 0._assignments/3._sess7/carObject.py
@@ -6,4 +6,34 @@ class Car:
         self.bhp = bhp
         self.mph = mph
 
+    @property
+    def make(self):
+        return self.make
     
+    @property
+    def model(self):
+        return self.model
+    
+    @property
+    def bhp(self):
+        return self.bhp
+    
+    @property
+    def mph(self):
+        return self.mph
+    
+    @make.setter
+    def make(self, m):
+        self.make = m
+    
+    @model.setter
+    def model(self, m):
+        self.model = m
+    
+    @bhp.setter
+    def bhp(self, b):
+        self.bhp = b
+    
+    @mph.setter
+    def mph(self, m):
+        self.mph = m
